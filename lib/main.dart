dd6ac7f57f54bdffde2817975c45a4e2a238ecb8
Jones Vitolo <jsvitolo@gmail.com>
2019-05-02 21:32:10 -0300

    Widget - Text

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,19 @@ class MyApp extends StatelessWidget {
 class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(width: 50, height: 50, color: Colors.red),
+    return Center(
+      child: Container(
+        width: 300,
+        height: 50,
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Text(
+          "Super Text",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+      ),
     );
   }
 }
