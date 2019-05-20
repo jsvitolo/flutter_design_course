b0cb998e989a38f687f881d5d4181cb932419552
Jones Vitolo <jsvitolo@gmail.com>
2019-05-20 19:40:05 -0300

    RaiseButton

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         body: HomeWidget(),
       ),
@@ -23,17 +21,23 @@ class HomeWidget extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Container(
-            height: 300,
+          RaisedButton(
+            onPressed: () {
+              print("RaisedButton");
+            },
+            child: Text("Click button"),
             color: Colors.blue,
           ),
-          Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
+          RaisedButton.icon(
+            onPressed: () {
+              print("RaisedButton.icon");
+            },
+            icon: Icon(Icons.android),
+            label: Text("Click button"),
           )
         ],
       ),
